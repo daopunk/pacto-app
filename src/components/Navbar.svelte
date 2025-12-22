7a639ef9694dc3ee7b5c4b9eac18eb204526a6c6
daopunk <cifrar@protonmail.com>
2025-12-22 17:57:03 -0500

    feat: nav/tab button

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
+  import Tab from './Tab.svelte';
 </script>
 
 <!-- Left side vertical navbar -->
 
-<div class="navbar"></div>
+<div class="navbar">
+  <div style="display: flex; justify-content: center;">
+    <div class="tab-list">
+      <Tab label="Home" active={true} />
+      <Tab label="Servers" />
+      <Tab label="Explore" />
+      <Tab label="Settings" />
+    </div>
+  </div>
+</div>
+
+<style>
+  .navbar {
+    width: 100px;
+    height: 100%;
+    background-color: #2b2c2c;
+  }
+</style>
