b77c844e13fe08d450c4580f3d3f3cb86caa8a5f
daopunk <cifrar@protonmail.com>
2026-01-06 19:21:42 -0500

    feat: community channels

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,13 +1,29 @@
 <script lang="ts">
   import Tab from './Tab.svelte';
   import settingsIcon from '../icons/settings.svg';
+  import { communities, activeCommunityId } from '../stores/app';
+
+  function selectCommunity(communityId: string) {
+    $activeCommunityId = communityId;
+  }
 </script>
 
 <div class="navbar">
   <div class="tab-list">
-    <Tab label="Home" />
-    <Tab label="Servers" />
-    <Tab label="Explore" />
+    {#each $communities as community}
+      <div 
+        on:click={() => selectCommunity(community.id)}
+        on:keydown={(e) => e.key === 'Enter' && selectCommunity(community.id)}
+        role="button"
+        tabindex="0"
+      >
+        <Tab 
+          label={community.name} 
+          image={community.image}
+          active={$activeCommunityId === community.id}
+        />
+      </div>
+    {/each}
   </div>
   <div class="tab-list bottom">
     <Tab label="Settings" image={settingsIcon} />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/core";
+  import { onMount } from 'svelte';
   import Navbar from '../components/Navbar.svelte';
+  import CommunityNavbar from '../components/CommunityNavbar.svelte';
+  import { activeCommunityId, activeChannelId } from '../stores/app';
+
+  // Set default active community on mount
+  onMount(() => {
+    $activeCommunityId = 'community-1';
+    $activeChannelId = 'channel-1';
+  });
 </script>
 
 <main class="container">
   <Navbar />
+  <CommunityNavbar />
 </main>
